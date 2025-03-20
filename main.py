426e56d08b47d5cb364e93cae30689c245647af9
Yasmin <yasmiin.vargass@gmail.com>
2025-03-20 12:00:49 -0300

    created snake class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen, time
+from snake import Snake
 
 screen = Screen()
 
@@ -7,32 +8,18 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-starting_position = [(0,0), (-20,0), (-40,0)]
-
-segments = []
-
-for position in starting_position:
-    new_segment = Turtle(shape="square")
-    new_segment.penup()
-    new_segment.color("white")
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+snake = Snake()
 game_is_on = True
 
 while game_is_on:
     try:
         screen.update()
         time.sleep(0.1)
-
-        for segment_num in range(len(segments) - 1, 0, -1):
-            new_x = segments[segment_num - 1].xcor()
-            new_y = segments[segment_num - 1].ycor()
-            segments[segment_num].goto(new_x, new_y)
-        segments[0].forward(50)
-        segments[0].left(90)
+        snake.move()
 
     except Exception as e:
             print(f"Erro detectado: {e}")
             game_is_on = False  # Encerra o loop caso haja erro
-screen.exitonclick()
+
+# screen.exitonclick()
+
